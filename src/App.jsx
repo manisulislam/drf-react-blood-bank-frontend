7c246008e22502a5a905ce310be9bee0d054e6eb
anisul islam <anisulislam980340@gmail.com>
2024-03-10 06:41:52 +0600

    react initial setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom"
+import {Home,SignUp,LogIN,Profile,VerifyEmail,ForgetPassword} from "./components/index"
+
+function App() {
+  
+
+  return (
+    <>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Home></Home>}/>
+        <Route path="/sign_up" element={<SignUp></SignUp>}/>
+        <Route path="/login" element={<LogIN></LogIN>}/>
+        <Route path="/dashboard" element={<Profile></Profile>}/>
+        <Route path="/otp/verify" element={<VerifyEmail></VerifyEmail>}/>
+        <Route path="/forget_password" element={<ForgetPassword></ForgetPassword>}/>
+      </Routes>
+     </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/index.jsx
@@ -0,0 +1,16 @@
+import Home from "./Home"
+import SignUp from "./SignUp"
+import LogIN from "./LogIn"
+import Profile from "./Profile"
+import VerifyEmail from "./VerifyEmail"
+import ForgetPassword from "./ForgetPassword"
+
+
+export {
+    Home,
+    SignUp,
+    LogIN,
+    Profile,
+    VerifyEmail,
+    ForgetPassword
+}
