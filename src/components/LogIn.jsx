1050e057db789ea7468d5f0ed8f0537a5799f9cb
anisul islam <anisulislam980340@gmail.com>
2024-03-10 10:54:43 +0600

    login done

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,23 +1,88 @@
-
+import { useState } from "react"
+import Swal from 'sweetalert2'
+import { useNavigate } from "react-router-dom"
+import axios from 'axios'
 
 const LogIn = () => {
+  const navigate=useNavigate()
+  const [loginData, setLoginData]=useState({
+    email:"",
+    password:""
+  })
+
+  const handleChange=(e)=>{
+    e.preventDefault()
+    setLoginData({
+      ...loginData,
+    [e.target.name]:e.target.value
+    })
+  }
+  const {email, password}=loginData
+  const [error, setError] = useState("")
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!email || !password) {
+      setError("All fields are required")
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: error,
+        showConfirmButton: false,
+        timer: 3000
+      });
+
+    }
+    else {
+      const res = await axios.post('http://127.0.0.1:8000/api/v1/auth/login/', loginData)
+
+      if (res.status === 200) {
+        navigate("/dashboard")
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: res.data.message,
+          showConfirmButton: false,
+          timer: 3000
+        });
+        const response=res.data
+        const user={
+          "email":response.email,
+          "names":response.full_name
+
+        }
+        localStorage.setItem(JSON.stringify(user))
+        localStorage.setItem(JSON.stringify(response.access_token))
+        localStorage.setItem(JSON.stringify(response.refresh_token))
+        
+        setError("")
+        setLoginData({
+          email: "",
+          password: "",
+         
+        })
+      }
+
+    }
+  }
+
   return (
     <>
      <div>
         <div className="max-w-md mx-auto my-10 bg-white p-6 rounded-md shadow-lg">
           <h2 className="text-2xl font-semibold text-center mb-6">Login</h2>
 
-          <form  >
+          <form onSubmit={handleSubmit} >
 
             <div className="mb-4">
 
               <label htmlFor="email" className="block text-gray-700">Email Address:</label>
               <input
                 type="email"
-
+                value={email}
+                onChange={handleChange}
                 id="email"
                 name="email"
-                required
+                
                 className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-300 rounded-md py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
             </div>
 
@@ -25,10 +90,12 @@ const LogIn = () => {
               <label htmlFor="password" className="block text-gray-700">Password:</label>
               <input
                 type="password"
+                value={password}
+                onChange={handleChange}
 
                 id="password"
                 name="password"
-                required
+                
                 className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-300 rounded-md py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
             </div>
            
